177a19da6f799436a6936412c028ff7c5184f5b8
aysnigdha <anzuman.yasmin@gmail.com>
2026-01-12 01:28:17 +0600

    allure result report adding

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("cypress");
-
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
       // implement node event listeners here
     },
       video: true, // ✅ Enable video recording
