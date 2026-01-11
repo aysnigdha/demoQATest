d0aef3c81158b23bc4465a74e6fef92bf11b1c4f
aysnigdha <anzuman.yasmin@gmail.com>
2026-01-12 00:16:01 +0600

    fix actions

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+      video: true, // ✅ Enable video recording
+      screenshotOnRunFailure: true, // optional: only take screenshots on failures
+      screenshotsFolder: 'cypress/screenshots',
+      videosFolder: 'cypress/videos'
   },
 });
